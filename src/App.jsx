c722b8f195fd4d333c21bfd25e634f46d2a1ef40
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-07-03 00:49:51 +0600

    Search function implemented with debounce

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Search from "./components/Search";
 import { useState, useEffect } from "react";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState("");
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm]);
 
   const API_BASE_URL = "https://api.themoviedb.org/3";
   const API_KEY = import.meta.env.VITE_TMDB_API_ACCESS_TOKEN;
@@ -20,11 +23,15 @@ function App() {
       Authorization: `Bearer ${API_KEY}`,
     },
   };
-  const fetchMovies = async () => {
+
+  const fetchMovies = async (query = "") => {
     setLoading(true);
     setErrorMessage("");
+    console.log(`Fetching movies with query: ${query}`);
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error("Failed to fetch movies");
@@ -45,8 +52,8 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
   return (
     <main>
       <div className="pattern" />
